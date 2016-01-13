05a9724bac12df43be770d9e4ae7a4bbf8ab1b3d
Victor Escorcia <escorciav@gmail.com>
2016-01-13 18:04:55 +0300

    [python] Function to generate text-files for C3D binaries
    This function generates text-files sources required for C3D input-layer.
    
    - Include dummy test that checks output generation and directory
      creation.

--- python/test/test_utils.py
+++ python/test/test_utils.py
@@ -1,4 +1,39 @@
+import os
+import shutil
+import tempfile
+from subprocess import check_output
+
 import utils
+
+
+def test_c3d_input_file_generator():
+    filename = 'not_existent_file'
+    summary = utils.c3d_input_file_generator(filename, '')
+    assert summary['success'] == False
+
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        f.write('my_video 50 0 22 0\nmy_video 50 10 33 1\n' +
+                'myvideo2 30 0 15 2\n')
+        filename = f.name
+    summary = utils.c3d_input_file_generator(filename, [filename + '.in',
+                                                        filename + '.out'],
+                                             output_folder=filename + '_dir')
+    assert summary['success'] == True
+    assert summary['pctg-skipped-segments'] == (1.0/3)
+    assert summary['ratio-clips-segments'] == (4.0/3)
+    # dummy test to double check that output has the right number of clips
+    assert os.path.isfile(filename + '.in') == True
+    assert check_output(['wc', '-l', filename + '.in']).split(' ')[0] == '4'
+    assert os.path.isfile(filename + '.out') == True
+    assert check_output(['wc', '-l', filename + '.out']).split(' ')[0] == '4'
+    assert os.path.isdir(filename + '_dir') == True
+    os.remove(filename)
+    os.remove(filename + '.in')
+    os.remove(filename + '.out')
+    shutil.rmtree(filename + '_dir')
+    return None
+
+# Tests of general purpose functions
 
 
 def test_count_frames():
--- python/utils.py
+++ python/utils.py
@@ -1,6 +1,95 @@
+import os
 from subprocess import check_output
 
 import cv2
+import numpy as np
+import pandas as pd
+
+
+def c3d_input_file_generator(filename, output_file, t_size=16, step_size=8,
+                             output_folder=None, mkdir=True):
+    """Generate textfile(s) used by C3D code
+
+    Parameters
+    ----------
+    filename : string
+        fullpath of CSV file (space separated of 5 columns without header) with
+        data about the videos to process.
+    output_file : string or list
+        list of output files to generate
+    t_size : int, optional
+        size of temporal field of your C3D network.
+    step_size : int, optional
+        control how dense do you plan to extract clips of your long video.
+    output_folder : string, optional
+        fullpath of folder to allocate outputs. Pass it to generate output-file
+        for extract-image-features.
+    mkdir : bool, optional
+        create folder to allocate outputs for C3D extract-image-features
+
+    Outputs
+    -------
+    summary : dict
+        statistics about the process
+
+    """
+    summary = dict(success=False, output=None)
+    col_names = ['video-name', 'num-frame', 'i-frame', 'duration', 'label']
+    try:
+        df = pd.read_csv(filename, sep=' ', header=None, names=col_names)
+    except:
+        return summary
+    if isinstance(output_file, str):
+        output_file = [output_file]
+
+    n_segments = df.shape[0]
+    idx_bool_keep = df['duration'] >= t_size
+    # Compute number of clips and from where to extract clips from each
+    # activity-segment
+    n_clips = ((df['duration'] - t_size) / step_size + 1).astype('int')
+    init_end_frame = np.concatenate(
+        [np.arange(df.loc[i, 'i-frame'],
+                   df.loc[i, 'i-frame'] + df.loc[i, 'duration'],
+                   step_size)[:n_clips[i]]
+         for i in df.index[idx_bool_keep]])
+
+    # Create new data frame with data required for C3D input layer
+    idx_keep_expanded = np.repeat(np.array(df.index[idx_bool_keep]),
+                                  n_clips[idx_bool_keep])
+    df_c3d = pd.DataFrame({1: df.loc[idx_keep_expanded, 'video-name'],
+                           2: init_end_frame,
+                           3: df.loc[idx_keep_expanded, 'label']})
+    try:
+        df_c3d.to_csv(output_file[0], sep=' ', header=False, index=False)
+    except:
+        return summary
+
+    # Dump outpu file
+    if len(output_file) > 1 and isinstance(output_folder, str):
+        skip_mkdir = False
+        try:
+            sr_out = (output_folder + os.path.sep +
+                      df.loc[idx_keep_expanded, 'video-name'].astype(str) +
+                      os.path.sep + init_end_frame.astype(str))
+            sr_out.to_csv(output_file[1], header=None, index=False)
+        except:
+            skip_mkdir = True
+
+        if not skip_mkdir:
+            summary['output'] = output_file[1]
+            if mkdir:
+                for i in sr_out:
+                    os.makedirs(i)
+
+    # Update summary
+    summary['success'] = True
+    summary['pctg-skipped-segments'] = ((n_segments - idx_bool_keep.sum()) *
+                                        1.0 / n_segments)
+    summary['ratio-clips-segments'] = (idx_keep_expanded.size * 1.0 /
+                                       n_segments)
+    return summary
+
+# General purpose functions
 
 
 def count_frames(filename, method=None):
