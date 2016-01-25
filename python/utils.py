ed563bdb7a68c618000ac3740ae254d07b20cbf6
Victor Escorcia <escorciav@gmail.com>
2016-01-25 17:14:08 +0300

    [python] Levenhstein distance function
    Disclaimer: this is a third-party implementation. A unit-test is
    welcome.

--- python/utils.py
+++ python/utils.py
@@ -241,6 +241,36 @@ def get_clip(filename, i_frame, duration):
     return np.stack(clip)
 
 
+def levenshtein_distance(s1, s2):
+    """Compute Levenshtein distance btw two strings
+
+    Note
+    ----
+    Taken from wikibooks.org-wiki-Algorithm_Implementation
+
+    """
+    if len(s1) < len(s2):
+        return levenshtein_distance(s2, s1)
+
+    # len(s1) >= len(s2)
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = range(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            # j+1 instead of j since previous_row and current_row are one
+            # character longer than s2
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
+
+
 def video_duration(filename):
     """Return frame-rate of video
 
