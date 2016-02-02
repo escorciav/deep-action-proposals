388df98379e008a0ff069cefab5796dd3a02ccf4
Victor Escorcia <escorciav@gmail.com>
2016-02-02 17:23:38 +0300

    [python] Include output with number of annotations per segment
    Save a loop over list of annotations including the number of
    annotations per segment. An argument is included to control the number
    of outputs.

--- python/data_generation.py
+++ python/data_generation.py
@@ -1,36 +1,44 @@
 import numpy as np
+import pandas as pd
 
+from baseline import TempPriorsNoScale
 from utils import sampling_with_uniform_groups
-from utils import segment_intersection
+from utils import segment_format, segment_intersection
 
 RATIO_INTERVALS = [0, 0.05, 0.15, 0.5, np.inf]
+REQ_INFO_CP = ['video-name', 'f-init', 'n-frames', 'video-frames']
 
 
 def generate_segments(t_size, l_size, annotations, cov_edges=RATIO_INTERVALS,
-                      i_thr=0.5, rng_seed=None):
+                      i_thr=0.5, rng_seed=None, return_annot=True):
     """Sample segments from a video
 
     Parameters
     ----------
     t_size : int
-        Size of the temporal window
+        Size of the temporal window.
     l_size : int
-        Size of the video
+        Size of the video.
     annotations : ndarray
-        2-dim array with annotations of video, format: [m x 2:=[init, end]]
+        2-dim array with annotations of video, format: [m x 2:=[init, end]].
     cov_edges : ndarray or list
-        1-dim array with intervals for discretize coverage
+        1-dim array with intervals for discretize coverage.
     i_thr : float, optional
         Threshold over intersection to consider that action appears inside a
-        segment
+        segment.
     rng_seed : int, optional
+    return_annot : bool, optional
+        Return two extra outputs (new_annotations and n_annotations).
 
     Outputs
     -------
     segments : ndarray
-    2-dim array of selected segments for a video, format: [n x 2:=[init, end]]
+        2-dim array of selected segments for a video,
+        format: [n x 2:=[init, end]].
     new_annotations : list
-        container for annotations inside the segment. length's list equal to n
+        container for annotations inside the segment. length's list equal to n.
+    n_annotations : ndarray
+        1-dim array of size n with number of annotations inside ith-segment.
 
     """
     rng = np.random.RandomState()
@@ -53,7 +61,14 @@ def generate_segments(t_size, l_size, annotations, cov_edges=RATIO_INTERVALS,
     idx_samples = rng.permutation(idx_samples)
 
     # Should valif i_segments have intersection >=0.5?
-    new_annotations = [None] * len(idx_samples)
-    for i, v in enumerate(idx_samples):
-        new_annotations[i] = i_segments[idx_mask[:, v], v, :]
-    return segments[idx_samples, :], new_annotations
+    if return_annot:
+        new_annotations = [None] * len(idx_samples)
+        n_annotations = np.zeros(len(idx_samples))
+        for i, v in enumerate(idx_samples):
+            new_annotations[i] = i_segments[idx_mask[:, v], v, :]
+            n_annotations = new_annotations[i].shape[0]
+        return segments[idx_samples, :], new_annotations, n_annotations
+
+    return segments[idx_samples, :]
+
+
