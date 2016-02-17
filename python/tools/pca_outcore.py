1afb48efdd5ce570d8c5b2786ce422f654ebe122
Victor Escorcia <escorciav@gmail.com>
2016-02-17 17:30:41 +0300

    [tools] Radical refactoring on inputs for pca-out-of-core
    It assumes that c3d features of one set (train, val, test) of a
    dataset are in one HDF5-file. It also computes the mean and the matrix
    X.T * X assuming that we can read all the features of a video in one
    pass.
    Note: this change break the compatibility with previous input.

--- python/tools/pca_outcore.py
+++ python/tools/pca_outcore.py
@@ -5,79 +5,52 @@ PCA done via matrix multiplication out-of-core. It is here just to be
 informative i.e. hostile and full of dependencies parsing of inputs.
 
 """
-import os
 import time
 
+import h5py
 import hickle as hkl
 import numpy as np
 
-from utils import c3d_stack_feature
-from data_generation import load_files
-
-C3D_DIR = 'data/thumos14/c3d/val/'
-REFFILE = 'data/experiments/thumos14/a01/train_ref.lst'
+THUMOS14_VAL = 'data/thumos14/c3d/val_c3d_temporal.hdf5'
 
 
-def read_feature(frame, dirname=C3D_DIR):
-    return c3d_stack_feature(dirname, [frame]).astype(np.float32)
-
-
-def read_reffile(filename):
-    """Load reference lst-file dump by data_generation.dump_files
-    """
-    _, df, _ = load_files(ref_file=filename)
-    return df
-
-
-def main(ref_file, t_size=16, t_stride=8, feat_dim=4096, dirname=C3D_DIR,
-         log_loop=10000):
-    print time.ctime(), 'start: input parsing'
-    df = read_reffile(ref_file)
-
-    # create dataframe with df, t
-    n = df.shape[0]
-    T = df.loc[:, 'duration'].min()
-    n_feat = (T - t_size) / t_stride + 1
-    frames = np.empty((n * n_feat), dtype=int)
-    for i, v in df.iterrows():
-        t_init = v['f-init']
-        frames[i*n_feat:(i+1)*n_feat] = np.arange(
-            t_init, t_init + T - 1, t_stride)[:n_feat]
-    video_name = df['video-name'].repeat(n_feat)
-    print time.ctime(), 'finish: input parsing'
+def main(h5file=THUMOS14_VAL, t_size=16, t_stride=8, feat_dim=4096,
+         source='c3d_features', log_loop=500000):
+    print time.ctime(), 'start: loading hdf5'
+    fid = h5py.File(h5file, 'r')
+    print time.ctime(), 'finish: loading hdf5'
 
     # Compute mean
     print time.ctime(), 'start: compute mean'
-    x_mean = np.zeros((1, feat_dim), dtype=np.float32)
-    for i in xrange(frames.size):
-        v = video_name.iloc[i]
-        filename = os.path.join(dirname, v)
-        x_mean += read_feature(frames[i], filename)
+    x_mean, n = np.zeros((1, feat_dim), dtype=np.float32), 0
+    for i, v in fid.iteritems():
+        feat = v[source][:]
+        n += feat.shape[0]
+        x_mean += feat.sum(axis=0)
     x_mean /= n
     print time.ctime(), 'finish: compute mean'
 
     # Compute A.T A
     print time.ctime(), 'start: out-of-core matrix multiplication'
+    j, n_videos = 0, len(fid.keys())
     ATA = np.zeros((feat_dim, feat_dim), dtype=np.float32)
-    for i in xrange(frames.size):
-        v = video_name.iloc[i]
-        filename = os.path.join(dirname, v)
-        xi = read_feature(frames[i], filename)
-        xi_ = xi - x_mean
-        ATA += np.dot(xi_.T, xi_)
+    for i, v in fid.iteritems():
+        feat = v[source][:]
+        feat_ = feat - x_mean
+        ATA += np.dot(feat_.T, feat_)
+        j += 1
 
-        if i % log_loop == 0:
-            print time.ctime(), 'Iteration {}/{}'.format(i, n * n_feat)
+        if j % log_loop == 0:
+            print time.ctime(), 'Iteration {}/{}'.format(j, n_videos)
     print time.ctime(), 'finish: out-of-core matrix multiplication'
 
     # SVD
     print time.ctime(), 'start: SVD in memory'
-    U, S, V = np.linalg.svd(ATA)
+    U, S, _ = np.linalg.svd(ATA)
     print time.ctime(), 'finish: SVD in memory'
 
     print time.ctime(), 'serializing ...'
-    hkl.dump({'x_mean': x_mean, 'U': U, 'S': S, 'V': V}, 'pca.hkl')
-
+    hkl.dump({'x_mean': x_mean, 'U': U, 'S': S}, 'pca_val_annot_thumos14.hkl')
 
 if __name__ == '__main__':
-    main(REFFILE)
+    main()
