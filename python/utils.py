6447ac0bfa18cef8d7fb59e2087eb6533b587481
cabaf <fabiancabaheilbron@gmail.com>
2016-02-18 00:58:09 +0300

    [python] Added feature concat pooling.

--- python/utils.py
+++ python/utils.py
@@ -389,6 +389,52 @@ def feature_1dpyramid(x, levels=0, pool_type='mean', norm=True, unit=False):
     return pyr_feat
 
 
+def feature_1dconcat(x, n=8, pool_type='mean', norm=True, unit=False):
+    """Compute a 1d pyramid representation of a feature vector
+    Parameters
+    ----------
+    x : ndarray.
+        [m x d] array of features. m is the number of features and d is the
+        dimensionality of the feature space.
+    n : int
+        Number of chunks.
+    pool_type : str.
+        Pooling strategy over a bunch of features.
+    norm : bool.
+        Normalize each region before concatenate them.
+    unit : bool.
+        Normalize the final input vector.
+    Outputs
+    -------
+    [d * n] ndarray with concat feature of x.
+    """
+    m, d = x.shape
+    arr = [np.empty(d) for i in range(n)]
+    pool_type = pool_type.lower()
+
+    edges = np.ones(n + 1, dtype=int) * 1.0 / n
+    edges[0] = 0
+    edges = np.round(np.cumsum(edges) * m).astype(int)
+    for j in range(n):
+        if pool_type == 'mean':
+            arr[j][...] = x[edges[j]:edges[j + 1], :].mean(axis=0)
+        elif pool_type == 'max':
+            arr[j][...] = x[edges[j]:edges[j + 1], :].max(axis=0)
+        else:
+            raise ValueError('Unknown pooling type {}'.format(pool_type))
+
+        if norm:
+            feat_norm = np.sqrt((arr[j] ** 2).sum())
+            if feat_norm == 0:
+                feat_norm = 1.0
+            arr[j] /= feat_norm
+
+    concat_feat = np.hstack(arr)
+    if unit:
+        return concat_feat / n
+    return concat_feat
+
+
 # Video utilities
 def count_frames(filename, method=None, ext='*.jpg'):
     """Count number of frames of a video
